8aaffcc8e224a7f537ceeba839939899bb36be74
Niranjan Reddy Palli <89531809+NiranjanReddyPalli@users.noreply.github.com>
2021-11-20 21:38:30 +0530

    Update Covid19_DA.sql

--- Covid-19_Project/Covid19_DA.sql
+++ Covid-19_Project/Covid19_DA.sql
@@ -1,6 +1,6 @@
-/* ##COVID-19 Data Analysis
+/* COVID-19 Data Analysis
 
-#Skills used : Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types */
+Skills used : Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types */
 
 
 use covid;
